dc10144462c95197f4de566b4dac9eb3da29c1b3
scohen-trinity <scohen@trinity.edu>
2023-11-11 11:48:53 -0600

    changed userController.scala to include proper imports and commented out testMethods because it does not return anything on Action right now and updated memoryModelFit.scala so that userPass is a var to be updated with more users and userInfo can be updated with a var as well as changed the Tuple type to a Tuple4 with explicity typing to compile, should be pretty readable, and finally commented out updateWeight for right now so that the program compiles because I did not want to debug it.

--- server/app/controllers/userController.scala
+++ server/app/controllers/userController.scala
@@ -1,12 +1,16 @@
 package controllers
 
-import memoryModelFit._
+import models.memoryModelFit
+import javax.inject._
+import play.api.mvc._
+import play.api.i18n._
+import java.lang.ProcessBuilder.Redirect
 
 @Singleton
 class userController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-    def testMethods = Action {
+    // def testMethods = Action {
 
-    }
+    // }
 
 
 }
--- server/app/models/memoryModelFit.scala
+++ server/app/models/memoryModelFit.scala
@@ -3,32 +3,32 @@ package models
 import collection.mutable
 
 object memoryModelFit{
-    private val userPass = mutable.Map[String, String]("samc" -> "drowssap", "setho" -> "password", "oliviab" -> "123", "samuelp"-> " ") 
+    private var userPass = mutable.Map[String, String]("samc" -> "drowssap", "setho" -> "password", "oliviab" -> "123", "samuelp"-> " ") 
     private val goals = Seq[String] ("Gain Muscle", "Lose Weight", "Stay Healthy")
-    private val userInfo = mutable.Map[String, Tuple]("samc" -> (180, 72, goals[0], 4))
+    private var userInfo = mutable.Map[String, Tuple4[Int, Int, String, Int]]("samc" -> Tuple4(180, 72, goals(0), 4))
 
     def validateUser(username: String, password: String): Boolean = {
         userPass.get(username).map(_ == password).getOrElse(false)
     }
 
     def createUser(username: String, password: String): Boolean = {
-        if (users.contains(username)) false else{
-            users(username) = password
+        if (userPass.contains(username)) false else{
+            userPass(username) = password
             true
         }
     }
 
     def updatePassword(username: String, oldPass: String, newPass: String): Boolean = {
-        if (validateUser(username, oldPass) == True) {
+        if (validateUser(username, oldPass) == true) {
             userPass(username) = newPass 
             true
         }
         else false
     }
 
-    def updateWeight(username: String, newWeight: String): Boolean = {
-        userInfo(username)._1 = newWeight
-        true
-    }
+    // def updateWeight(username: String, newWeight: String): Boolean = {
+    //     userInfo(username)._1 = newWeight
+    //     true
+    // }
     
 }
