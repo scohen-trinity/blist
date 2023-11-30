570409915d7a463cee269e104be72cdc1ff3ed23
scohen-trinity <scohen@trinity.edu>
2023-11-30 14:47:42 -0600

    added in future handling and pair programmed with Olivia and Seth, changed InMemoryModel error when it was changed from an object to a class

--- server/app/controllers/userController.scala
+++ server/app/controllers/userController.scala
@@ -6,32 +6,56 @@ import play.api.mvc._
 import play.api.i18n._
 import play.api.libs.json._
 import java.lang.ProcessBuilder.Redirect
+import scala.concurrent.Future
 
 @Singleton
 class userController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
     // def testMethods = Action {
 
     // }
-    def validate = Action { implicit request =>
-        request.body.asJson.map { ud => 
-            ud.as[UserData] match {
-                case userData: UserData =>
-                    if(memoryModelFit.validateUser(userData.username, userData.password)) {
-                        Ok(Json.toJson(true))
-                    } else {
-                        Ok(Json.toJson(false))
+
+    implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+
+    val memInstance = new models.MemoryModelFit()
+
+    implicit val userDataReads = Json.reads[UserData]
+    implicit val userDataWrites = Json.writes[UserData]
+    
+    def validate = Action.async { implicit request =>
+        request.body.asJson.map { ud =>
+            Json.fromJson[UserData](ud) match {
+                case JsSuccess(ld, path) => {
+                    memInstance.validateUser(ld.username, ld.password).flatMap{innerValue => 
+                        if(innerValue) {
+                            Future.successful(Ok(Json.toJson(true)))
+                        } else {
+                            Future.successful(Ok(Json.toJson(false)))
+                        }
                     }
-                case _ => 
-                    BadRequest("Invalid JSON format")
+                }
+                case e @ JsError(_) => Future.successful(Ok(Json.toJson(false)))
             }
         }.getOrElse {
-            BadRequest("Expecting JSON data")
+            Future.successful(Ok(Json.toJson(false)))
         }    
     }
 
+    // def validate = Action { implicit request =>
+    //     request.body.asJson.map { ud => 
+    //         ud.as[UserData] match {
+    //             case userData: UserData =>
+    //                 memInstance.validateUser(userData.username, userData.password).flatMap{innerValue => 
+    //                     if(innerValue) {
+    //                         Future.successful(Ok(Json.toJson(true)))
+    //                     } else {
+    //                         Future.successful(Ok(Json.toJson(false)))
+    //                     }
+    //                 }
+    //             case _ => 
+    //                 BadRequest("Invalid JSON format")
+    //         }
+    //     }  
+    // }
 
-}
 
-class tests {
-    println(memoryModelFit.updatePassword("samc", "drowssap", "test"))
 }
